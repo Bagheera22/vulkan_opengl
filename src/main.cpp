9e93feb3221ae063462e582571227348b8f5cb08
roger <correur1@gmail.com>
2017-09-24 19:23:48 +0200

    compile all

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,15 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/hash.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtx/hash.hpp>
 
 #include <iostream>
 #include <stdexcept>
